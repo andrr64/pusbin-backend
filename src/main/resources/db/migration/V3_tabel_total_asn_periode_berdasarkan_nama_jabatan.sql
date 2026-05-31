78a61f4b189a1f2348b50a2cbcc5aeec333cf3b4
Derza Andreas <derzandrr03@gmail.com>
2026-05-31 13:12:41 +0700

    tambah tabel baru

---
+++ src/main/resources/db/migration/V3_tabel_total_asn_periode_berdasarkan_nama_jabatan.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS total_asn_periode_by_nama_jabatan (
+    id bigserial primary key, 
+    jumlah_asn integer,
+    periode date not null,
+    id_jabatan integer REFERENCES jabatan(id)
+)
